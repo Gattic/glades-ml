c1e338cca256b4ebb2e0b45fbe9458f89fe25b77
ai-mystic-coder <ai.mystic.coder@gmail.com>
2024-12-09 21:38:41 -0500

    ohe test case (#31)
    * Initial
    
    * Added test case for mapFeatureSpace

---
+++ unit-tests/Backend/Machine Learning/ohe-test.h
@@ -0,0 +1,20 @@
+// Confidential, unpublished property of Robert Carneiro
+
+// The access and distribution of this material is limited solely to
+// authorized personnel.  The use, disclosure, reproduction,
+// modification, transfer, or transmittal of this work for any purpose
+// in any form or by any means without the written permission of
+// Robert Carneiro is strictly prohibited.
+#ifndef _UT_OHE
+#define _UT_OHE
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <unistd.h>
+
+void OHEUnitTest();
+
+#endif
