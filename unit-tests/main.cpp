a3f28cb53467a649ce675787bd3963e059c7d6d5
tigran <tigranminas@hotmail.com>
2025-08-14 13:05:29 +0400

    called test with validation and timing series cases

--- unit-tests/main.cpp
+++ unit-tests/main.cpp
@@ -21,8 +21,9 @@ int main(int argc, char* argv[])
 
 	if (argc == 1)
 	{
-	    NNCFUnitTestNew();
+	    NNCFUnitTestValidation();
         /*
+	    NNCFUnitTestNew();
 	    NNUnitTest();
 //	    NNCFUnitTest();
 //	    PCAUnitTest();
