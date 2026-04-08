d0802f5462207a41504d3a70ca8ce3b62d2bb181
cabral450 <roceso1337@gmail.com>
2026-04-08 01:53:04 -0400

    LLM updates

--- unit-tests/Backend/Machine Learning/nn-save-load-test.cpp
+++ unit-tests/Backend/Machine Learning/nn-save-load-test.cpp
@@ -367,6 +367,9 @@ public:
 		return true;
 	}
 
+	virtual bool hasTokenIdInput() const { return true; }
+	virtual bool hasTokenIdExpectedOutput() const { return true; }
+
 	virtual unsigned int getTrainSize() const { return static_cast<unsigned int>(trainTok.size()); }
 	virtual unsigned int getTestSize() const { return static_cast<unsigned int>(testTok.size()); }
 	virtual unsigned int getFeatureCount() const { return 1u; }
